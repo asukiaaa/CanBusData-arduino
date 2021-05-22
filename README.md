3bcb45ac9d09c9dd406abf61eae1737150fccb8e
Asuki Kono <asukiaaa@gmail.com>
2021-05-22 14:53:02 +0900

    Add initial files

---
+++ README.md
@@ -0,0 +1,7 @@
+# CanBusData-arduino (CanBusData_asukiaaa)
+
+A library for arduino to define data types for Can bus.
+
+# License
+
+MIT
---
+++ src/CanBusData_asukiaaa.hpp
@@ -0,0 +1,11 @@
+#ifndef _CAN_BUS_Data_ASUKIAAA_HPP_
+#define _CAN_BUS_Data_ASUKIAAA_HPP_
+
+namespace CanBusData_asukiaaa {
+
+class Frame {
+};
+
+};
+
+#endif
